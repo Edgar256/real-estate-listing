e990d2b2cc81822dd538f4ac8dfcfae49bb17272
edgar256 <tinka.edgar@gmail.com>
2023-01-31 03:27:45 +0300

    added AJAx for searching propert by name and location id

--- listing.php
+++ listing.php
@@ -10,7 +10,7 @@ $sql = "SELECT properties.*, locations.id AS location_id, locations.name AS loca
 $check_properties_list = $conn->query($sql);
 
 // Include search fille
-require('./utils/search_property.php');
+// require('./utils/search_property.php');
 
 ?>
 <html lang="en">
@@ -32,7 +32,33 @@ require('./utils/search_property.php');
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous"></script>
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
     <title>Equitable Property Group</title>
+
+    <script>
+        $(document).ready(function () {
+            $("#search-form").submit(function (e) {
+                e.preventDefault();
+                var property_name = $("property_name").val();
+                var location_id = $('#location_id:selected').val()
+                $.ajax({
+                    url: "./utils/search_property.php",
+                    type: "post",
+                    data: $('#search-form').serialize(),
+                    success: function (response) {
+                        console.log(response);
+                        $("#results").html(response);
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        // handle error
+                        console.log({ jqXHR, textStatus, errorThrown });
+                    }
+
+                })
+            });
+        });
+    </script>
 </head>
 
 <body>
@@ -48,7 +74,7 @@ require('./utils/search_property.php');
                 <div class="d-flex">
                     <?php echo $entries_err; ?>
                 </div>
-                <form class="row gx-3 gy-2 align-items-center" method="post">
+                <form class="row gx-3 gy-2 align-items-center" method="post" id="search-form">
                     <div class="col-sm-5">
                         <!-- <label for="specificSizeInputName">Search by Name</label> -->
                         <input type="text" class="form-control" id="property_name" name="property_name"
@@ -56,7 +82,7 @@ require('./utils/search_property.php');
                     </div>
                     <div class="col-sm-5">
                         <!-- <label for="specificSizeInputName">Search by Location</label> -->
-                        <select name="location_id" id="" class="form-control <?php if (!empty($location_err))
+                        <select name="location_id" id="location_id" class="form-control <?php if (!empty($location_err))
                             echo "border-danger"; ?>">
                             <option value="">Select Property Location</option>
                             <?php
@@ -77,14 +103,14 @@ require('./utils/search_property.php');
                         </select>
                     </div>
                     <div class="col-sm-2">
-                        <button type="submit" class="btn btn-primary w-100" name="form_search_property">Submit</button>
+                        <input type="submit" class="btn btn-primary w-100" name="submit" value="Submit" />
                     </div>
                 </form>
             </div>
             <!-- end search section -->
 
             <!-- start listing section -->
-            <div class="container d-flex flex-wrap pb-5 mb-5">
+            <div class="container d-flex flex-wrap pb-5 mb-5" id="results">
                 <?php
                 // redirect to user login if session is expired
                 if (!$_SESSION['auth_active']) {
@@ -93,56 +119,47 @@ require('./utils/search_property.php');
                         window.location.href = "./auth/user-login.php";
                     }, 1000);</script>';
                 }
-                if ($_SESSION['role'] == "USER") {
 
-                    if ($check_properties_list->num_rows > 0) {
-                        while ($row = $check_properties_list->fetch_assoc()) {
-                            $title = mb_convert_case($row["title"], MB_CASE_TITLE, "UTF-8");
-                            $id = $row["id"];
-                            $description = $row["property_description"];
-                            $price = $row["price"];
-                            $location = mb_convert_case($row["location_name"], MB_CASE_TITLE, "UTF-8");
-                            $imageData = $row['property_image'];
-                            $imageData = base64_encode($imageData);
-                            $datePosted = $row["reg_date"];
-                            $property_type_name = mb_convert_case($row["property_type_name"], MB_CASE_TITLE, "UTF-8");
+                // Dispalying default property list              
+                if ($check_properties_list->num_rows > 0) {
+                    while ($row = $check_properties_list->fetch_assoc()) {
+                        $title = mb_convert_case($row["title"], MB_CASE_TITLE, "UTF-8");
+                        $id = $row["id"];
+                        $description = $row["property_description"];
+                        $price = $row["price"];
+                        $location = mb_convert_case($row["location_name"], MB_CASE_TITLE, "UTF-8");
+                        $imageData = $row['property_image'];
+                        $imageData = base64_encode($imageData);
+                        $datePosted = $row["reg_date"];
+                        $property_type_name = mb_convert_case($row["property_type_name"], MB_CASE_TITLE, "UTF-8");
 
-                            echo '<div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-12 p-1">
-                                <div class="card">
-                                    <span style="background-image: url(data:image/jpeg;base64,' . $imageData . ');
-                                        background-size: cover;
-                                        background-repeat: no-repeat;
-                                        background-position: center center;
-                                        width: 100%;
-                                        border-radius: 3px 3px 0px 0px;
-                                        min-height: 200px;">
-                                    </span>
-                                    <div class="card-body">
-                                        <p class="text_muted"><small><i>Posted : ' . date("F j, Y, g:i a", strtotime($datePosted)) . '</i></small></p>
-                                        <h5 class="card-title">' . $title . '</h5>
-                                        <h6>Location : ' . $location . '</h6>
-                                        <h6>Property Type : ' . $property_type_name . '</h6>
-                                        <h6>Price: USD ' . number_format($price) . '</h6>
-                                        <p class="card-text">
-                                            ' . $description . '
-                                        </p>
-                                        <a href="./property-profile.php?id=' . $id . '" class="btn btn-primary w-100">View More</a>
-                                    </div>
+                        echo '<div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-12 p-1">
+                            <div class="card">
+                                <span style="background-image: url(data:image/jpeg;base64,' . $imageData . ');
+                                    background-size: cover;
+                                    background-repeat: no-repeat;
+                                    background-position: center center;
+                                    width: 100%;
+                                    border-radius: 3px 3px 0px 0px;
+                                    min-height: 200px;">
+                                </span>
+                                <div class="card-body">
+                                    <p class="text_muted"><small><i>Posted : ' . date("F j, Y, g:i a", strtotime($datePosted)) . '</i></small></p>
+                                    <h5 class="card-title">' . $title . '</h5>
+                                    <h6>Location : ' . $location . '</h6>
+                                    <h6>Property Type : ' . $property_type_name . '</h6>
+                                    <h6>Price: USD ' . number_format($price) . '</h6>
+                                    <p class="card-text">
+                                        ' . $description . '
+                                    </p>
+                                    <a href="./property-profile.php?id=' . $id . '" class="btn btn-primary w-100">View More</a>
                                 </div>
-                            </div>';
-                        }
-
-                    } else {
-                        echo "<div class='w-100 text-center py-5 display-1'>No Results found</div>";
+                            </div>
+                        </div>';
                     }
-
-
-                } else {
-                    header("location:javascript://history.go(-1)");
                 }
-
+                
                 ?>
-
             </div>
             <!-- end lisitng section -->
 
@@ -153,18 +170,7 @@ require('./utils/search_property.php');
 
         </div>
     </div>
-    <script>
-        // When the form is submitted
-        $("form").submit(function (event) {
-            event.preventDefault(); // prevent the form from submitting
-            alert(event.target.value )
 
-            // Get the value of the input field
-            var inputValue = $("#property_name").val();
-            alert(inputValue);
-
-        });
-    </script>
 </body>
 
 </html>
