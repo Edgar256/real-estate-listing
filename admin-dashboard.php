5485a476a51bbac7c1c3304cd2d176af751de899
edgar256 <tinka.edgar@gmail.com>
2023-02-07 18:24:41 +0300

    ADMIN UPDATES

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -15,6 +15,7 @@
 
   <!-- Custom styles for this template -->
   <link href="./css/dashboard.css" rel="stylesheet" />
+
 </head>
 
 <body>
@@ -32,30 +33,13 @@
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
         <div
           class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h1 class="h2">Dashboard</h1>
-          <div class="btn-toolbar mb-2 mb-md-0">
-            <div class="btn-group me-2">
-              <button type="button" class="btn btn-sm btn-outline-secondary">
-                Share
-              </button>
-              <button type="button" class="btn btn-sm btn-outline-secondary">
-                Export
-              </button>
-            </div>
-            <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
-              <span data-feather="calendar" class="align-text-bottom"></span>
-              This week
-            </button>
-          </div>
+          <h1 class="h2">Dashboard</h1>          
         </div>
 
         <div class="d-flex">
           <div class="col-4 p-2">
-            <div class="alert alert-primary alert-dismissible fade show">
-              <i class="bi bi-person-circle"></i>
-              <!-- <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-              <span class="glyphicon">&#xe077;</span>
-              <i class="bi bi-search"></i> -->
+            <div class="alert alert-primary alert-dismissible fade show d-flex">
+              <span data-feather="user" class="align-text-bottom w-25" style="height: 70px;"></span>
               <div class="w-75">
                 <h3 class="">Users</h3>
                 <h6><a href="./users-listing.php"
@@ -64,9 +48,8 @@
             </div>
           </div>
           <div class="col-4 p-2">
-            <div class="alert alert-warning alert-dismissible fade show">
-              <i class="bi bi-person-circle"></i>
-              <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+            <div class="alert alert-warning alert-dismissible fade show d-flex">
+              <span data-feather="command" class="align-text-bottom w-25" style="height: 70px;"></span>
               <div class="w-75">
                 <h3 class="">Managers</h3>
                 <h6><a href="./managers-listing.php"
@@ -76,9 +59,8 @@
             </div>
           </div>
           <div class="col-4 p-2">
-            <div class="alert alert-secondary alert-dismissible fade show">
-              <i class="bi bi-person-circle"></i>
-              <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+            <div class="alert alert-secondary alert-dismissible fade show d-flex">
+              <span data-feather="terminal" class="align-text-bottom w-25" style="height: 70px;"></span>
               <div class="w-75">
                 <h3 class="">Admins</h3>
                 <h6><a href="./admins-listing.php"
