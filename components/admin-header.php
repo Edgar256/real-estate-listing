507b6ce4aa0ca9fe7509b862379a13a44595eb6b
edgar256 <tinka.edgar@gmail.com>
2023-02-08 11:07:05 +0300

    link fix

--- components/admin-header.php
+++ components/admin-header.php
@@ -1,5 +1,5 @@
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="./index.php">
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="./admin-dashboard.php">
     <img src="./images/logo-white.svg" height="40" width="250" alt="" />
   </a>
   <!-- <a class="navbar-brand" href="../index.php">
