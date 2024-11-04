f4e1a0b14a1f2afc4ff2ee475cc62315dce50fd8
Edgar Tinkamanyire <tinka.edgar@gmail.com>
2024-11-04 22:55:55 +0300

    Added .env file and set up loading values from .env file

--- config/credentials.php
+++ config/credentials.php
@@ -1,9 +1,34 @@
 <?php
 
-// DATABASE CREDENTIALS
-define("DB_HOST", 'localhost');     // DATABASE HOST
-define("DB_USER", 'root');          // DATABASE USER
-define("DB_PASSWORD", '');          // DATABASE PASSWORD
-define("DB_NAME", 'real_estate_database'); // DATABASE NAME
+function loadEnv($path)
+{
+    if (!file_exists($path)) {
+        throw new Exception('.env file not found');
+    }
+
+    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) {
+            continue;
+        }
+
+        list($name, $value) = explode('=', $line, 2);
+        $name = trim($name);
+        $value = trim($value);
+
+        if (!array_key_exists($name, $_ENV)) {
+            $_ENV[$name] = $value;
+        }
+    }
+}
+
+// Load .env file
+loadEnv(__DIR__ . '/../.env');
+
+// Define constants using environment variables
+define("DB_HOST", $_ENV['DB_HOST']);
+define("DB_USER", $_ENV['DB_USER']);
+define("DB_PASSWORD", $_ENV['DB_PASSWORD']);
+define("DB_NAME", $_ENV['DB_NAME']);
 
 ?>
